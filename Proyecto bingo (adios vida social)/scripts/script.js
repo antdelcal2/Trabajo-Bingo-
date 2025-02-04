a4355d4f9fdab23cda4155593b764308f033000b
antdelcal2 <antdelcal2@alu.edu.gva.es>
2025-02-04 08:36:41 +0100

    Add files via upload

---
+++ Proyecto bingo (adios vida social)/scripts/script.js
@@ -0,0 +1,81 @@
+const NumeroAleatorio1 = Math.floor(Math.random() * 3) + 1;  // Genera entre 1 y 3
+document.getElementById('columna1').textContent = NumeroAleatorio1;
+
+const NumeroAleatorio2 = Math.floor(Math.random() * 3) + 4;  // Genera entre 4 y 6
+document.getElementById('columna1-2').textContent = NumeroAleatorio2;
+
+const NumeroAleatorio3 = Math.floor(Math.random() * 3) + 7;  // Genera entre 7 y 9
+document.getElementById('columna1-3').textContent = NumeroAleatorio3;
+
+const NumeroAleatorio4 = Math.floor(Math.random() * 3) + 11;  // Genera entre 11 y 13
+document.getElementById('columna2').textContent = NumeroAleatorio4;
+
+const NumeroAleatorio5 = Math.floor(Math.random() * 3) + 14;  // Genera entre 14 y 16
+document.getElementById('columna2-2').textContent = NumeroAleatorio5;
+
+const NumeroAleatorio6 = Math.floor(Math.random() * 3) + 17;  // Genera entre 17 y 19
+document.getElementById('columna2-3').textContent = NumeroAleatorio6;
+
+const NumeroAleatorio7 = Math.floor(Math.random() * 3) + 21;  // Genera entre 21 y 23
+document.getElementById('columna3').textContent = NumeroAleatorio7;
+
+const NumeroAleatorio8 = Math.floor(Math.random() * 3) + 24;  // Genera entre 24 y 26
+document.getElementById('columna3-2').textContent = NumeroAleatorio8;
+
+const NumeroAleatorio9 = Math.floor(Math.random() * 3) + 28;  // Genera entre 28 y 30
+document.getElementById('columna3-3').textContent = NumeroAleatorio9;
+
+const NumeroAleatorio10 = Math.floor(Math.random() * 3) + 31;  // Genera entre 31 y 33
+document.getElementById('columna4').textContent = NumeroAleatorio10;
+
+const NumeroAleatorio11 = Math.floor(Math.random() * 3) + 34;  // Genera entre 34 y 36
+document.getElementById('columna4-2').textContent = NumeroAleatorio11;
+
+const NumeroAleatorio12 = Math.floor(Math.random() * 3) + 38;  // Genera entre 38 y 40
+document.getElementById('columna4-3').textContent = NumeroAleatorio12;
+
+const NumeroAleatorio13 = Math.floor(Math.random() * 3) + 41;  // Genera entre 41 y 43
+document.getElementById('columna5').textContent = NumeroAleatorio13;
+
+const NumeroAleatorio14 = Math.floor(Math.random() * 3) + 45;  // Genera entre 45 y 47
+document.getElementById('columna5-2').textContent = NumeroAleatorio14;
+
+const NumeroAleatorio15 = Math.floor(Math.random() * 3) + 48;  // Genera entre 48 y 50
+document.getElementById('columna5-3').textContent = NumeroAleatorio15;
+
+const NumeroAleatorio16 = Math.floor(Math.random() * 3) + 51;  // Genera entre 51 y 53
+document.getElementById('columna6').textContent = NumeroAleatorio16;
+
+const NumeroAleatorio17 = Math.floor(Math.random() * 3) + 55;  // Genera entre 55 y 57
+document.getElementById('columna6-2').textContent = NumeroAleatorio17;
+
+const NumeroAleatorio18 = Math.floor(Math.random() * 3) + 58;  // Genera entre 58 y 60
+document.getElementById('columna6-3').textContent = NumeroAleatorio18;
+
+const NumeroAleatorio19 = Math.floor(Math.random() * 3) + 61;  // Genera entre 61 y 63
+document.getElementById('columna7').textContent = NumeroAleatorio19;
+
+const NumeroAleatorio20 = Math.floor(Math.random() * 3) + 64;  // Genera entre 64 y 66
+document.getElementById('columna7-2').textContent = NumeroAleatorio20;
+
+const NumeroAleatorio21 = Math.floor(Math.random() * 3) + 67;  // Genera entre 67 y 69
+document.getElementById('columna7-3').textContent = NumeroAleatorio21;
+
+const NumeroAleatorio22 = Math.floor(Math.random() * 3) + 70;  // Genera entre 70 y 72
+document.getElementById('columna8').textContent = NumeroAleatorio22;
+
+const NumeroAleatorio23 = Math.floor(Math.random() * 3) + 73;  // Genera entre 73 y 75
+document.getElementById('columna8-2').textContent = NumeroAleatorio23;
+
+const NumeroAleatorio24 = Math.floor(Math.random() * 3) + 76;  // Genera entre 76 y 78
+document.getElementById('columna8-3').textContent = NumeroAleatorio24;
+
+const NumeroAleatorio25 = Math.floor(Math.random() * 3) + 79;  // Genera entre 79 y 81
+document.getElementById('columna9').textContent = NumeroAleatorio25;
+
+const NumeroAleatorio26 = Math.floor(Math.random() * 3) + 82;  // Genera entre 82 y 84
+document.getElementById('columna9-2').textContent = NumeroAleatorio26;
+
+const NumeroAleatorio27 = Math.floor(Math.random() * 5) + 85;  // Genera entre 85 y 89
+document.getElementById('columna9-3').textContent = NumeroAleatorio27;
+
